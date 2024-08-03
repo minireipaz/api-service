612bb2b9bab2a5c38156c7f7770685eb8b31c6b9
jose luis <2064537+sosan@users.noreply.github.com>
2024-08-03 17:29:03 +0200

    kafkaconfig and DI
    Signed-off-by: jose luis <2064537+sosan@users.noreply.github.com>

--- pkg/di/di.go
+++ pkg/di/di.go
@@ -3,16 +3,32 @@ package di
 import (
 	"minireipaz/pkg/config"
 	"minireipaz/pkg/domain/services"
+	"minireipaz/pkg/infra/brokerclient"
+	"minireipaz/pkg/infra/httpclient"
 	"minireipaz/pkg/infra/redisclient"
 	"minireipaz/pkg/interfaces/controllers"
 )
 
-func InitDependencies() (*controllers.WorkflowController, *services.AuthService) {
+func InitDependencies() (*controllers.WorkflowController, *services.AuthService, *controllers.UserController) {
+	configZitadel := config.NewZitaldelEnvConfig()
+	kafkaConfig := config.NewKafkaEnvConfig()
+
 	// init autentication
-	config := config.NewZitaldelEnvConfig()
-	authContext := controllers.NewAuthContext(config)
+	authContext := controllers.NewAuthContext(configZitadel)
 	authContext.GetAuthController()
 	authService := authContext.GetAuthService()
+
+	userRedisClient := redisclient.NewRedisClient()
+	// userHttpClient := httpclient.NewUserClientHTTP()
+	userHTTPClient := &httpclient.HttpClientImpl{}
+	userBrokerClient := brokerclient.NewBrokerClient(kafkaConfig)
+
+	repoUserRedis := redisclient.NewUserRedisRepository(userRedisClient)
+	repoUserHTTP := httpclient.NewUserClientHTTP(userHTTPClient) /// .UserHTTPRepository(userHTTPClient)
+  repoUserBroker := brokerclient.NewUserKafkaRepository(userBrokerClient)
+
+	userService := services.NewUserService(repoUserHTTP, repoUserRedis, repoUserBroker)
+	userController := controllers.NewUserController(userService)
 
 	workflowRedisClient := redisclient.NewRedisClient()
 	repo := redisclient.NewWorkflowRepository(workflowRedisClient)
@@ -20,6 +36,6 @@ func InitDependencies() (*controllers.WorkflowController, *services.AuthService)
 	workflowService := services.NewWorkflowService(repo, idService)
 	workflowController := controllers.NewWorkflowController(workflowService, authService)
 
-	return workflowController, authService
+	return workflowController, authService, userController
 
 }

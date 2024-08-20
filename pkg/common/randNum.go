9bae4211bfc26c186e07cd980c3b0ec2792b15d9
jose luis <2064537+sosan@users.noreply.github.com>
2024-08-20 04:47:02 +0200

    incremental
    Signed-off-by: jose luis <2064537+sosan@users.noreply.github.com>

--- pkg/common/randNum.go
+++ pkg/common/randNum.go
@@ -17,8 +17,8 @@ func RandomDuration(max, min time.Duration, i int) time.Duration {
 
 	nBig, err := rand.Int(rand.Reader, big.NewInt(int64(rangeDuration)))
 	if err != nil {
-		return time.Second * 1
+		return time.Second
 	}
 
-	return min + time.Duration(nBig.Int64()) + models.SleepOffset*time.Duration(i)
+	return (min + time.Duration(nBig.Int64()) + models.SleepOffset) * time.Duration(i*int(time.Second))
 }

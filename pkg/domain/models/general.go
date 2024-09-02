aef87489958ec8e5d46980fe5d80f83557aa6c95
jose luis <2064537+sosan@users.noreply.github.com>
2024-09-02 22:43:13 +0200

    time format
    Signed-off-by: jose luis <2064537+sosan@users.noreply.github.com>

--- pkg/domain/models/general.go
+++ pkg/domain/models/general.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 const (
-	LayoutTimestamp       = "2006-01-02T15:04:05Z07:00"
+	LayoutTimestamp       = time.RFC3339 // format "2006-01-02T15:04:05Z07:00"
 	MaxAttempts           = 11
 	MinRangeSleepDuration = 100 * time.Millisecond // min range time wait offset
 	MaxRangeSleepDuration = 500 * time.Millisecond // max range time wait offset

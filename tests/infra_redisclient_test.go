59256f960b0c493296af28db6bb51ee26411e304
jose luis <2064537+sosan@users.noreply.github.com>
2024-09-02 22:48:36 +0200

    modified test needs to improve
    Signed-off-by: jose luis <2064537+sosan@users.noreply.github.com>

--- tests/infra_redisclient_test.go
+++ tests/infra_redisclient_test.go
@@ -24,9 +24,9 @@ func TestRedisClient_checkAndSetWorkflow(t *testing.T) {
 	}
 
 	workflow := &models.Workflow{
-		Sub:          "user123",
-		WorkflowName: "test-workflow",
-		UUID:         uuid.New(),
+		UserID: "user123",
+		Name:   "test-workflow",
+		UUID:   uuid.New().String(),
 	}
 
 	tests := []struct {
@@ -38,11 +38,11 @@ func TestRedisClient_checkAndSetWorkflow(t *testing.T) {
 		{
 			name: "successful transaction",
 			setup: func() {
-				mock.ExpectHExists("workflows:all", workflow.UUID.String()).SetVal(false)
-				mock.ExpectHExists(fmt.Sprintf("users:%s", workflow.Sub), workflow.WorkflowName).SetVal(false)
+				mock.ExpectHExists("workflows:all", workflow.UUID).SetVal(false)
+				mock.ExpectHExists(fmt.Sprintf("users:%s", workflow.UserID), workflow.Name).SetVal(false)
 				mock.ExpectTxPipeline()
-				mock.ExpectHSet(fmt.Sprintf("users:%s", workflow.Sub), workflow.WorkflowName, workflow.UUID.String()).SetVal(1)
-				mock.ExpectHSet("workflows:all", workflow.UUID.String(), workflow.Sub).SetVal(1)
+				mock.ExpectHSet(fmt.Sprintf("users:%s", workflow.UserID), workflow.Name, workflow.UUID).SetVal(1)
+				mock.ExpectHSet("workflows:all", workflow.UUID, workflow.UserID).SetVal(1)
 				mock.ExpectTxPipelineExec()
 			},
 			wantErr: false,
@@ -50,7 +50,7 @@ func TestRedisClient_checkAndSetWorkflow(t *testing.T) {
 		{
 			name: "UUID already exists",
 			setup: func() {
-				mock.ExpectHExists("workflows:all", workflow.UUID.String()).SetVal(true)
+				mock.ExpectHExists("workflows:all", workflow.UUID).SetVal(true)
 			},
 			wantErr: true,
 			errMsg:  "UUID already exists",
@@ -58,8 +58,8 @@ func TestRedisClient_checkAndSetWorkflow(t *testing.T) {
 		{
 			name: "workflow name already exists for this user",
 			setup: func() {
-				mock.ExpectHExists("workflows:all", workflow.UUID.String()).SetVal(false)
-				mock.ExpectHExists(fmt.Sprintf("users:%s", workflow.Sub), workflow.WorkflowName).SetVal(true)
+				mock.ExpectHExists("workflows:all", workflow.UUID).SetVal(false)
+				mock.ExpectHExists(fmt.Sprintf("users:%s", workflow.UserID), workflow.Name).SetVal(true)
 			},
 			wantErr: true,
 			errMsg:  "workflow name already exists for this user",
@@ -70,7 +70,7 @@ func TestRedisClient_checkAndSetWorkflow(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.setup()
 			err := r.Client.Watch(r.Ctx, func(tx *redis.Tx) error {
-				return r.CheckAndSetWorkflow(ctx, tx, workflow)
+				return r.CheckAndModifyWorkflow(ctx, tx, workflow, "set")
 			})
 			if tt.wantErr {
 				assert.Error(t, err)

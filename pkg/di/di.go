03850c40af1d5f8a545e55ae26f1044cc0c511b0
jose luis <2064537+sosan@users.noreply.github.com>
2024-12-13 19:51:12 +0100

    httpclient with timeouts
    Signed-off-by: jose luis <2064537+sosan@users.noreply.github.com>

--- pkg/di/di.go
+++ pkg/di/di.go
@@ -3,6 +3,7 @@ package di
 import (
 	"minireipaz/pkg/config"
 	"minireipaz/pkg/dimodel"
+	"minireipaz/pkg/domain/models"
 	"minireipaz/pkg/domain/services"
 	"minireipaz/pkg/infra/brokerclient"
 	"minireipaz/pkg/infra/httpclient"
@@ -21,7 +22,7 @@ func InitDependencies() *dimodel.Dependencies {
 	authController := authContext.GetAuthController()
 
 	userRedisClient := redisclient.NewRedisClient()
-	userHTTPClient := &httpclient.ClientImpl{}
+	userHTTPClient := httpclient.NewClientImpl(models.TimeoutRequest)
 	userBrokerClient := brokerclient.NewBrokerClient(kafkaConfig)
 
 	repoUserRedis := redisclient.NewUserRedisRepository(userRedisClient)
@@ -31,7 +32,7 @@ func InitDependencies() *dimodel.Dependencies {
 	userService := services.NewUserService(repoUserHTTP, repoUserRedis, repoUserBroker)
 	userController := controllers.NewUserController(userService)
 
-	credentialHTTPClient := &httpclient.ClientImpl{}
+	credentialHTTPClient := httpclient.NewClientImpl(models.TimeoutRequest)
 	credentialRedisClient := redisclient.NewRedisClient()
 	credentialBrokerClient := brokerclient.NewBrokerClient(kafkaConfig)
 	redisCredentialRepo := redisclient.NewCredentialRedisRepository(credentialRedisClient)
@@ -42,7 +43,7 @@ func InitDependencies() *dimodel.Dependencies {
 	credentialService := services.NewCredentialService(googleCredentialRepo, facebookCredentialRepo, redisCredentialRepo, repoCredentialBroker, repoCredentialHTTP)
 	credentialController := controllers.NewCredentialController(credentialService, authService)
 
-	workflowHTTPClient := &httpclient.ClientImpl{}
+	workflowHTTPClient := httpclient.NewClientImpl(models.TimeoutRequest)
 	repoWorkflowHTTP := httpclient.NewWorkflowClientHTTP(workflowHTTPClient, clickhouseConfig)
 	workflowRedisClient := redisclient.NewRedisClient()
 	workflowBrokerClient := brokerclient.NewBrokerClient(kafkaConfig)
@@ -52,19 +53,19 @@ func InitDependencies() *dimodel.Dependencies {
 	workflowService := services.NewWorkflowService(repoWorkflowRedis, repoWorkflowBroker, idService, repoWorkflowHTTP)
 	workflowController := controllers.NewWorkflowController(workflowService, credentialService, authService)
 
-	dashboardHTTPClient := &httpclient.ClientImpl{}
+	dashboardHTTPClient := httpclient.NewClientImpl(models.TimeoutRequest)
 	dashboardRepo := httpclient.NewDashboardRepository(dashboardHTTPClient, clickhouseConfig)
 	dashboardService := services.NewDashboardService(dashboardRepo)
 	dashboardController := controllers.NewDashboardController(dashboardService, authService)
 
-	actionsHTTPClient := &httpclient.ClientImpl{}
+	actionsHTTPClient := httpclient.NewClientImpl(models.TimeoutRequest)
 	actionsRedisClient := redisclient.NewRedisClient()
 	actionsBrokerClient := brokerclient.NewBrokerClient(kafkaConfig)
 	repoActionsRedis := redisclient.NewActionsRepository(actionsRedisClient)
 	repoActionsBroker := brokerclient.NewActionsKafkaRepository(actionsBrokerClient)
 	actionsRepo := httpclient.NewActionsClientHTTP(actionsHTTPClient, clickhouseConfig)
 	actionsService := services.NewActionsService(repoActionsRedis, repoActionsBroker, actionsRepo)
-	actionsController := controllers.NewActionsController(actionsService)
+	actionsController := controllers.NewActionsController(actionsService, authService)
 
 	return &dimodel.Dependencies{
 		WorkflowController:   workflowController,
